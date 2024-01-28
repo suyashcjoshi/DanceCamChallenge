65da74b1136357aa25d3761eaa7310662bc88aa4
Suyash Joshi <suyash@suyashjoshi.com>
2024-01-28 13:23:39 +0000

    Added background image and improved main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -7,6 +7,11 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame() 
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit();
     }
 }
